764f56d87b19777b6eba2b9a8d4c81cf07164121
Niklas Gollenstede <n.gollenstede@web.de>
2016-05-30 03:34:00 +0200

    allow an element as tabview tab icon

--- tabview/index.js
+++ tabview/index.js
@@ -6,25 +6,39 @@ const styles = {
 	default: `
 		.content, .tablist, .tab {
 			box-sizing: border-box;
+			font-family: "Segoe UI";
 		}
 		.content {
 			position: absolute;
 			overflow: auto;
 		}
+		.tabwrapper {
+			position: absolute;
+			overflow: hidden;
+		}
 		.tablist {
 			position: absolute;
+			top: 0px; right: 0px; bottom: 0px; left: 0px;
+			overflow: auto;
 		}
 		.tab {
 			border-color: transparent;
 			border-style: solid;
 			border-width: 0;
-			background-repeat: no-repeat;
-			background-position: center left;
 			transition-property: color, fill, background-color, border-color;
 			transition-duration: 0.21s;
 			cursor: pointer;
 			-webkit-user-select: none;
 			-moz-user-select: none;
+		}
+		.tab>.icon {
+			display: inline-block;
+			background-size: 100%;
+			background-repeat: no-repeat;
+			background-position: center center;
+			text-align: center;
+		}
+		.tab>.title {
 		}
 	`,
 	vertical: `
@@ -32,18 +46,30 @@ const styles = {
 			top: 0px; height: 100%;
 			right: 0px; width: calc(100% - 200px);
 		}
-		.tablist {
+		.tabwrapper {
 			top: 0px; height: 100%;
 			left: 0px; width: 200px;
+		}
+		.tablist {
 			padding-top: 54px;
+			overflow-y: scroll;
+			overflow-x: hidden;
+			margin-right: -17px;
 		}
 		.tab {
 			height: 54px;
 			line-height: 54px;
-			padding-left: 54px;
 			font-size: 24px;
 			border-left-width: 5px;
-			background-size: 48px;
+			padding: 3px;
+		}
+		.tab>.icon {
+			height: 48px;
+			width: 48px;
+		}
+		.tab>.title {
+			position: relative;
+			top: -15px;
 		}
 	`,
 	horizontal: `
@@ -51,19 +77,32 @@ const styles = {
 			left: 0px; width: 100%;
 			bottom: 0px; height: calc(100% - 40px);
 		}
-		.tablist {
+		.tabwrapper {
 			left: 0px; width: 100%;
 			top: 0px; height: 40px;
+		}
+		.tablist {
+			/*overflow-y: hidden;*/
+			/*overflow-x: scroll;*/
+			/*margin-bottom: -17px;*/
 		}
 		.tab {
 			float: left;
 			height: 100%;
 			line-height: 35px;
-			padding-left: 32px;
+			padding-left: 0px;
 			padding-right: 10px;
-			background-position-x: 10px;
-			background-size: 20px;
 			border-bottom-width: 5px;
+			padding: 6px;
+		}
+		.tab>.icon {
+			height: 24px;
+			width: 24px;
+		}
+		.tab>.title {
+			position: relative;
+			top: -7px;
+			padding-left: 4px;
 		}
 	`,
 	firefox: `
@@ -89,7 +128,9 @@ return class Home {
 		this.style = host.appendChild(document.createElement('style'));
 		this.style.scoped = true;
 		this.style.textContent = typeof style !== 'string' ? '' : styles.default + style.split(' ').map(style => styles[style]).join('\n');
-		this.tablist = host.appendChild(document.createElement('div'));
+		this.tabwrapper = host.appendChild(document.createElement('div'));
+		this.tabwrapper.classList = 'tabwrapper';
+		this.tablist = this.tabwrapper.appendChild(document.createElement('div'));
 		this.tablist.classList = 'tablist';
 		this.content = host.appendChild(content);
 		this.content.classList = 'content';
@@ -111,17 +152,22 @@ return class Home {
 
 	add({ id, title, icon, position = Infinity, data, }) {
 		const tab = this.tablist.insertBefore(document.createElement('div'), this.tablist.children[position]);
-		tab.dataset.id = id;
 		tab.className = 'tab';
-		tab.style.backgroundImage = `url(${ icon })`;
-		tab.textContent = title;
+		tab.dataset.id = id;
+		tab.icon = tab.appendChild(document.createElement('div'));
+		tab.icon.classList = 'icon';
+		setIcon(tab, icon);
+		tab._title = tab.appendChild(document.createElement('span'));
+		tab._title.classList = 'title';
+		tab._title.textContent = title;
 		tab.data = data;
 		tab.onclick = ({ button, }) => !button && (this.active = id);
 	}
 
 	set(props) {
 		const tab = this.get(props.id);
-		'title' in props && (tab.textContent = props.title);
+		'title' in props && (tab._title.textContent = props.title);
+		'icon' in props && setIcon(tab, props.icon);
 	}
 
 	remove(id) {
@@ -132,5 +178,15 @@ return class Home {
 		return this.tablist.querySelector('.tab[data-id="'+ id +'"]');
 	}
 };
+
+function setIcon(tab, icon) {
+	tab.icon.textContent = '';
+	tab.icon.style.backgroundImage = '';
+	if (typeof icon === 'string') {
+		tab.icon.style.backgroundImage = `url(${ icon })`;
+	} else if (icon instanceof Element) {
+		tab.icon.appendChild(icon);
+	}
+}
 
 });
