8124635949b9728297525bca76aa9c911ed593c2
Niklas Gollenstede <n.gollenstede@web.de>
2016-07-31 16:39:07 +0200

    add utils/attachAllContentScripts()

--- utils.js
+++ utils.js
@@ -1,4 +1,8 @@
-'use strict'; define('web-ext-utils/utils', function() {
+'use strict'; define('web-ext-utils/utils', [
+	'web-ext-utils/chrome'
+], function(
+	{ Tabs, }
+) {
 
 const escape = string => string.replace(/[\-\[\]\{\}\(\)\*\+\?\.\,\\\^\$\|\#]/g, '\\$&');
 
@@ -15,8 +19,38 @@ function matchPatternToRegExp(pattern) {
 	+')$');
 }
 
+/**
+ * Can be called during the extension startup to immediately attach all content scripts as they are specified in the 'manifest.json'.
+ * Supported keys for each content script are 'js', 'css', 'matches' and 'exclude_matches'. Globs are not supported.
+ * @param  {object}    options          Optional options object
+ * @param  {function}  options.cleanup  Optional function whose code is executed in each included tab before each content script to remove old versions of the script.
+ * @return {Promise([ natural, ])}      Promise that resolves once the cleanup function ran in all included tabs. The numbers are the number of tabs each content script is applied to.
+ *                                      Note, that the content scripts themselves have not necessarily been executed yet
+ */
+function attachAllContentScripts({ cleanup, } = { }) {
+	if (typeof (cleanup = cleanup || (() => void 0)) !== 'function') { throw new TypeError('"Cleanup" paramter must be a function or falsey'); }
+
+	return Tabs.query({ }).then(tabs => {
+		return Promise.all(chrome.runtime.getManifest().content_scripts.map(({ js, css, matches, exclude_matches, }) => {
+			const includes = (matches || [ ]).map(pattern => pattern === '<all_urls>' ? (/./) : matchPatternToRegExp(pattern));
+			const excludes = (exclude_matches || [ ]).map(matchPatternToRegExp);
+			return Promise.all(tabs.map(({ id, url, }) => {
+				if (!url || !includes.some(exp => exp.test(url)) || excludes.some(exp => exp.test(url))) { return; }
+				return Tabs.executeScript(id, { code: `(${ cleanup })();`, })
+				.then(() => {
+					css && css.forEach(file => chrome.tabs.insertCSS(id, { file, }));
+					js && js.forEach(file => chrome.tabs.executeScript(id, { file, }));
+					return true;
+				})
+				.catch(error => console.log('skipped tab', error)); // not allowed to execute
+			})).then(_=>_.filter(_=>_).length);
+		}));
+	});
+}
+
 return {
 	matchPatternToRegExp,
+	attachAllContentScripts,
 };
 
 });
