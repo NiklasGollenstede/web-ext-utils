851b0c0aae977d124adc7743d0b81800771d1b91
Niklas Gollenstede <n.gollenstede@web.de>
2016-12-09 05:01:17 +0100

    ignore chrome.storage.sync if it throws on .get() (e.g. in FF 52/53)

--- chrome/index.js
+++ chrome/index.js
@@ -1,7 +1,7 @@
 (function(global) { 'use strict'; const factory = function webExtUtils_chrome(exports) { // This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-const _chrome = typeof chrome !== 'undefined' && chrome;
-const _browser = typeof browser !== 'undefined' && browser;
+const _chrome = typeof chrome !== 'undefined' && global.chrome;
+const _browser = typeof browser !== 'undefined' && global.browser;
 const _api = _browser || _chrome;
 
 const cache = new WeakMap;
@@ -12,7 +12,7 @@ const rootUrl = _api.extension.getURL('.').slice(0, -1);
 const blink = rootUrl.startsWith('chrome');
 const opera = blink && (/ OPR\/\d+\./).test(ua); // TODO: is this safe to do?
 const vivaldi = blink && (/ Vivaldi\/\d+\./).test(ua); // TODO: is this safe to do?
-const google = blink && !opera && !vivaldi; // TODO: thst for Google Chrome specific api
+const google = blink && !opera && !vivaldi; // TODO: test for Google Chrome specific api
 const chromium = blink && !opera && !vivaldi && !google;
 
 const gecko = rootUrl.startsWith('moz');
@@ -37,22 +37,45 @@ const appVersion = (() => { switch (true) {
 	case (vivaldi):         return        (/Vivaldi\/((?:\d+.)*\d+)/).exec(ua)[1];
 	case (opera):           return            (/OPR\/((?:\d+.)*\d+)/).exec(ua)[1];
 	case (blink):           return (/Chrom(?:e|ium)\/((?:\d+.)*\d+)/).exec(ua)[1];
-	case (fennec): switch (false) {
+	case (fennec): switch (true) {
 		// TODO: keep up to date
-		case !(_api.pageAction && _api.pageAction.show): return '50.0';
+		case (global.CSS.supports('display', 'grid')): return '52.0'; // should use some _api key instead
+		case (_api.management && _api.management.getSelf): return '51.0';
+		case (_api.pageAction && _api.pageAction.show): return '50.0';
 		default: return '48.0';
 	} break;
-	case (firefox): switch (false) {
+	case (firefox): switch (true) {
 		// TODO: keep up to date
-		case !(_api.runtime.connectNative || _api.history && _api.history.getVisits): return '50.0'; // these require permissions
-		case !(_api.tabs.removeCSS): return '49.0';
-		case !(_api.commands.getAll): return '48.0';
-		case !(_api.tabs.insertCSS): return '47.0';
-		case !(_api.tabs.move): return '46.0';
+		case (global.CSS.supports('display', 'grid')): return '52.0'; // should use some _api key instead
+		case (_api.management && _api.management.getSelf): return '51.0';
+		case (_api.runtime.connectNative || _api.history && _api.history.getVisits): return '50.0'; // these require permissions
+		case (_api.tabs.removeCSS): return '49.0';
+		case (_api.commands.getAll): return '48.0';
+		case (_api.tabs.insertCSS): return '47.0';
+		case (_api.tabs.move): return '46.0';
 		default: return '45.0';
 	}
 	return '0';
 } })();
+
+
+return new Promise((resolve, reject) => { // if storage.sync is unavailable, use storage.local instead
+	const storage = gecko ? _browser.storage : wrapAPI(_api.storage);
+	if (!storage.sync) {
+		cloneStorage();
+	} else { try {
+		storage.sync.get('some_key')
+		.then(() => resolve(storage))
+		.catch(cloneStorage) // Throws in Firefox if storage.sync is disabled
+		.catch(reject);
+	} catch (_) { } }
+	function cloneStorage() {
+		const clone = Object.assign({ }, storage);
+		// console.info('chrome.storage.sync is unavailable, fall back to chrome.storage.local');
+		clone.sync = storage.local;
+		resolve(Object.freeze(clone));
+	}
+}).then(Storage => {
 
 /**
  * This is a flat copy of the window.chrome / window.browser API with the additional properties:
@@ -109,7 +132,7 @@ const Chrome = new Proxy(Object.freeze({
 		if (self.hasOwnProperty(key)) { return self[key]; }
 		throw new Error(`Unknown application "${ key }"`);
 	}, set() { }, }),
-	Storage: cloneLocal(gecko ? _browser.storage : wrapAPI(_api.storage))
+	Storage,
 }), { get(self, key) {
 	let value;
 	value = self[key]; if (value) { return value; }
@@ -117,7 +140,6 @@ const Chrome = new Proxy(Object.freeze({
 	key = key.replace(/^./, s => s.toLowerCase());
 	value = gecko ? _browser[key] : wrapAPI(_api[key]); if (value) { return value; }
 }, set() { }, });
-
 
 function getGlobalPort() {
 	if (messageHandler) { return messageHandler; }
@@ -133,6 +155,8 @@ function getGlobalPort() {
 
 	return (messageHandler = port);
 }
+
+return (Chrome); });
 
 // Deeply clones an object but replaces all functions with Promise-wrapped functions.
 function wrapAPI(api) {
@@ -157,14 +181,6 @@ function promisifyAll(api) {
 	return Object.freeze(clone);
 }
 
-function cloneLocal(storage) {
-	if (storage.sync) { return storage; }
-	storage = Object.assign({ }, storage);
-	// console.info('chrome.storage.sync is unavailable, fall back to chrome.storage.local');
-	storage.sync = storage.local;
-	return Object.freeze(storage);
-}
-
 function promisify(method, thisArg) {
 	return function() {
 		return new Promise((resolve, reject) => {
@@ -176,7 +192,4 @@ function promisify(method, thisArg) {
 	};
 }
 
-
-return (Chrome);
-
-}; if (typeof define === 'function' && define.amd) { define([ 'exports', ], factory); } else { const exp = { }, result = factory(exp) || exp; if (typeof exports === 'object' && typeof module === 'object') { module.exports = result; } else { global[factory.name] = result; } } })((function() { return this; })());
+}; if (typeof define === 'function' && define.amd) { define([ 'exports', ], factory); } else { const exp = { }, result = factory(exp) || exp; if (typeof exports === 'object' && typeof module === 'object') { module.exports = result; } else { global[factory.name] = result; } } })((function() { /* jshint strict: false */ return this; })());
