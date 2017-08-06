ac37e4534829f3a648cf6aaae223fd9ca8d3802f
Niklas Gollenstede <n.gollenstede@web.de>
2017-08-06 05:30:38 +0200

    fix publication

--- test/scripts/postinstall.js
+++ test/scripts/postinstall.js
@@ -17,16 +17,3 @@ function find(name) {
 		|| FS.pathExists(path +'/'+ name).then(_=>_ ? path +'/'+ name : null)
 	), Promise.resolve(null));
 }
-
-/*Promise.all([
-	{ module: 'pbq/require.js', target: 'require.js', },
-	{ module: 'multiport', target: 'port.js', },
-].map(({ module, target, }) => FS.lstat(lib(target)).catch(() => null).then(stat => {
-	if (stat && stat.isSymbolicLink()) {
-		return console.log(`Keeping symlink for ${ module } in ${ lib(target) }`);
-	}
-	return FS.copy(require.resolve(module), lib(target));
-}))).catch(error => {
-	console.error(error);
-	process.exit(1);
-});*/
